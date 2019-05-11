5212f38c362b12b6c00ac5e3cf50f0d1f0583c13
Bruno Dourado <brunomouradourado@gmail.com>
2019-05-11 13:36:48 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>
           React running on Docker! Made by Bruno Moura Dourado
         </p>
       </header>
